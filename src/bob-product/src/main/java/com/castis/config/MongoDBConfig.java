792a9aefcfb52e8ae6745b3ce0b1bf3585e6b3b1
bobmann <bobmann98@gmail.com>
2016-09-29 15:07:37 +0900

    modified mongodb config

--- src/bob-product/src/main/java/com/castis/config/MongoDBConfig.java
+++ src/bob-product/src/main/java/com/castis/config/MongoDBConfig.java
@@ -1,7 +1,10 @@
 package com.castis.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.data.mongodb.config.AbstractMongoConfiguration;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
@@ -11,20 +14,27 @@ import com.mongodb.MongoClient;
 @Configuration
 @ComponentScan
 @EnableMongoRepositories
+@PropertySource(value = {"classpath:bob-product.properties"})
 public class MongoDBConfig extends AbstractMongoConfiguration{
 	
-	 @Override
-	 protected String getDatabaseName() {
-	  return "products";
-	 }
+	@Autowired
+	private Environment env;
+	
+	@Override
+	protected String getDatabaseName() {
+		String dbName = env.getRequiredProperty("db.database");
+		return dbName;
+	}
 
-	 @Override
-	 public Mongo mongo() throws Exception {
-	  return new MongoClient("bobmann.asuscomm.com", 27017);
-	 }
+	@Override
+	public Mongo mongo() throws Exception {
+		String host = env.getRequiredProperty("db.host");
+		int port = Integer.parseInt(env.getRequiredProperty("db.port"));
+		return new MongoClient(host, port);
+	}
 	 
-	 @Override
-	 public String getMappingBasePackage() {
-	  return "com.castis";
-	 }
+	@Override
+	public String getMappingBasePackage() {
+		return "com.castis";
+	}
 }
